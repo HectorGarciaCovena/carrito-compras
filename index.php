63fdba5cf7d5dc39c310f0291a0b1106474bc1fe
HectorGarciaCovena <hectormgc.85@gmail.com>
2023-12-21 08:20:06 -0500

    segundo commit

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include 'Configuracion.php';
 <head>
     <title>Carrito de Compras</title>
     <meta charset="utf-8">
-    //Hacemos uso de framework bootstrap para el uso de estilos responsivos y bondades JS
+    
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
