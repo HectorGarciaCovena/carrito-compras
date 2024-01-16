cb09fc499d255152b2b51ccd777280e026960da5
HectorGarciaCovena <hectormgc.85@gmail.com>
2024-01-15 23:42:59 -0500

    Actualización

--- index.php
+++ index.php
@@ -8,12 +8,19 @@ include 'Configuracion.php';
     <title>Carrito de Compras</title>
     <meta charset="utf-8">
     
+    <!-- Enlazamos la web con la versión 3.3.7 de Bootstrap y sus dependencias (jQuery) a través de BootstrapCDN 
+    y Google CDN.-->
+    <!-- Aplicamos los estilos predefinidos de Bootstrap a la página.-->
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <!-- Enlazamos la página con la versión 1.12.4 de jQuery alojada en los servidores de Google.-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+    <!-- Enlazamos la página con el archivo JavaScript de Bootstrap.-->
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <style>
         .container {
-            padding: 20px;
+            padding: 10px;
         }
 
         .cart-link {
@@ -31,24 +38,30 @@ include 'Configuracion.php';
         <div class="panel panel-default">
             <div class="panel-heading">
 
+                <!-- MENU -->
                 <ul class="nav nav-pills">
-                    <li role="presentation" class="active"><a href="index.php">Inicio</a></li>
-                    <li role="presentation"><a href="VerCarta.php">Carrito de Compras</a></li>
-                    <li role="presentation"><a href="Pagos.php">Pagar</a></li>
-                    <li role="presentation"><a href="https://www.facebook.com/hectormiguel.garciacovena.9" target="_blank">Héctor García</a></li>
+                    <li role="presentacion" class="active"><a href="index.php">Inicio</a></li>
+                    <li role="presentacion"><a href="VerCarta.php">Carrito de Compras</a></li>
+                    <li role="presentacion"><a href="Pagos.php">Pagar</a></li>
+                    <li role="presentacion"><a href="https://www.facebook.com/hectormiguel.garciacovena.9" target="_blank">Héctor García</a></li>
                 </ul>
             </div>
 
+            <!-- CATÁLOGO DE PRODUCTOS -->
             <div class="panel-body">
                 <h1>Tienda de Productos</h1>
                 <a href="VerCarta.php" class="cart-link" title="Ver Carta"><i class="glyphicon glyphicon-shopping-cart"></i></a>
                 <div id="products" class="row list-group">
                     <?php
-                    //Rescatamos la consulta en una tabla
+                    
+                    //Extraemos los productos tabla "mis_productos" de la Base de Datos
                     $query = $db->query("SELECT * FROM mis_productos ORDER BY id DESC LIMIT 10");
                     if ($query->num_rows > 0) {
                         while ($row = $query->fetch_assoc()) {
                     ?>
+                            <!-- Mostramos cada producto en un contenedor de tipo "thumbnail"... -->
+                            <!-- ... con su respectivo detalle (imagen, nombre y precio) -->
+                            <!-- ... y almacena la información extraída en el array asociativo $row -->
                             <div class="item col-lg-4">
                                 <div class="thumbnail">
                                     <img src="<?php echo $row["image"]; ?>" alt="<?php echo $row["name"]; ?>" class="img-responsive">
